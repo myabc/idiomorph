aaff889351b4f853847f58d0118444141eaa26d1
Micah Geisel <micah@botandrose.com>
2026-08-29 12:16:21 +0200

    colocate all cross-realm tests into test/realm.js.

--- test/core.js
+++ test/core.js
@@ -1,13 +1,5 @@
 describe("Core morphing tests", function () {
   setup();
-  function makeForeign(htmlStr) {
-    let iframe = document.createElement("iframe");
-    getWorkArea().append(iframe);
-    let container = iframe.contentDocument.createElement("div");
-    container.innerHTML = htmlStr;
-    return container.firstElementChild;
-  }
-
   it("morphs outerHTML by default", function () {
     let initial = make("<button>Foo</button>");
     let finalSrc = "<button>Bar</button>";
@@ -78,81 +70,6 @@ describe("Core morphing tests", function () {
     );
   });
 
-  it("morphs outerHTML as content properly when argument is a node from another document", function () {
-    let initial = make("<button>Foo</button>");
-    let iframe = document.createElement("iframe");
-    getWorkArea().append(iframe);
-    let final = iframe.contentDocument.createElement("button");
-    final.textContent = "Bar";
-    Idiomorph.morph(initial, final, { morphStyle: "outerHTML" });
-    initial.outerHTML.should.equal("<button>Bar</button>");
-  });
-
-  it("syncs input value when newContent is a node from another document", function () {
-    let initial = make('<input type="text">');
-    initial.value = "Foo";
-    let iframe = document.createElement("iframe");
-    getWorkArea().append(iframe);
-    let final = iframe.contentDocument.createElement("input");
-    final.setAttribute("type", "text");
-    final.setAttribute("value", "Bar");
-    Idiomorph.morph(initial, final, { morphStyle: "outerHTML" });
-    initial.value.should.equal("Bar");
-  });
-
-  it("preserves id'd element when detached newContent is from another document", function () {
-    let initial = make(`<div><span id="s">Foo</span></div>`);
-    let span = initial.querySelector("span");
-    let final = makeForeign(`<div><div><span id="s">Foo</span></div></div>`);
-    final.remove();
-    Idiomorph.morph(initial, final);
-    initial.outerHTML.should.equal(
-      `<div><div><span id="s">Foo</span></div></div>`,
-    );
-    initial.querySelector("span").should.equal(span);
-  });
-
-  it("preserves id'd element when attached newContent is from another document", function () {
-    let initial = make(`<div><span id="s">Foo</span></div>`);
-    let span = initial.querySelector("span");
-    let final = makeForeign(`<div><div><span id="s">Foo</span></div></div>`);
-
-    Idiomorph.morph(initial, final);
-    initial.outerHTML.should.equal(
-      `<div><div><span id="s">Foo</span></div></div>`,
-    );
-    initial.querySelector("span").should.equal(span);
-  });
-
-  it("morphs template tag contents when newContent is from another document", function () {
-    let initial = make("<template data-old>Foo</template>");
-    let final = makeForeign("<template data-new>Bar</template>");
-    Idiomorph.morph(initial, final);
-    initial.outerHTML.should.equal('<template data-new="">Bar</template>');
-  });
-
-  it("syncs option selectedness when newContent is from another document", function () {
-    let parent = make(
-      `<div><select><option>0</option><option>1</option></select></div>`,
-    );
-    getWorkArea().append(parent);
-    let select = parent.querySelector("select");
-    let final = makeForeign(
-      `<select><option>0</option><option>1</option></select>`,
-    );
-    final.children[1].selected = true;
-    Idiomorph.morph(select, final);
-    select.selectedIndex.should.equal(1);
-  });
-
-  it("syncs textarea value when newContent is from another document", function () {
-    let initial = make("<textarea>Foo</textarea>");
-    initial.value = "dirty";
-    let final = makeForeign("<textarea>Bar</textarea>");
-    Idiomorph.morph(initial, final);
-    initial.value.should.equal("Bar");
-  });
-
   it("morphs children of an SVG-namespace template element normally", function () {
     let initial = make("<div><svg><template><g></g></template></svg></div>");
     let finalSrc = "<div><svg><template><rect></rect></template></svg></div>";
@@ -192,56 +109,6 @@ describe("Core morphing tests", function () {
     });
     initial.firstChild.getAttribute("lang").should.equal("b");
     initial.firstChild.firstChild.textContent.should.equal("New");
-  });
-
-  it("merges head element from another document", function () {
-    let iframe = document.createElement("iframe");
-    getWorkArea().append(iframe);
-    let foreignDocument = iframe.contentDocument;
-    foreignDocument.head.innerHTML = `<meta name="a"><title>Foo</title>`;
-    let newHead = foreignDocument.createElement("head");
-    newHead.innerHTML = `<title>Foo</title><meta name="a">`;
-    Idiomorph.morph(foreignDocument.head, newHead);
-    foreignDocument.head.innerHTML.should.equal(
-      `<meta name="a"><title>Foo</title>`,
-    );
-  });
-
-  it("ignores head element from another document when head.style is none", function () {
-    let iframe = document.createElement("iframe");
-    getWorkArea().append(iframe);
-    let foreignDocument = iframe.contentDocument;
-    foreignDocument.title = "Old";
-    let newHead = foreignDocument.createElement("head");
-    newHead.innerHTML = "<title>New</title>";
-    Idiomorph.morph(foreignDocument.head, newHead, {
-      head: { style: "none" },
-    });
-    foreignDocument.title.should.equal("Old");
-  });
-
-  it("preserves namespaced attributes when newContent is from another document", function () {
-    let initial = make(`<div><svg><use></use></svg></div>`);
-    let final = makeForeign(
-      `<div><svg><use xlink:href="#foo"></use></svg></div>`,
-    );
-    Idiomorph.morph(initial, final);
-    initial
-      .querySelector("use")
-      .getAttributeNS("http://www.w3.org/1999/xlink", "href")
-      .should.equal("#foo");
-  });
-
-  it("morphs a document from another realm properly", function () {
-    let iframe = document.createElement("iframe");
-    getWorkArea().append(iframe);
-    let foreignDocument = iframe.contentDocument;
-    foreignDocument.body.innerHTML = "<p>Foo</p>";
-    Idiomorph.morph(
-      foreignDocument,
-      "<html><head></head><body><p>Bar</p></body></html>",
-    );
-    foreignDocument.body.innerHTML.should.equal("<p>Bar</p>");
   });
 
   it("morphs outerHTML as content properly when argument is HTMLElementCollection with siblings", function () {
--- test/realm.js
+++ test/realm.js
@@ -9,6 +9,14 @@ describe("Cross-realm morphing tests", function () {
     return iframe;
   }
 
+  function makeForeign(htmlStr) {
+    let iframe = document.createElement("iframe");
+    getWorkArea().append(iframe);
+    let container = iframe.contentDocument.createElement("div");
+    container.innerHTML = htmlStr;
+    return container.firstElementChild;
+  }
+
   // chai hangs the test runner when it tries to inspect a cross-realm node, so assert on booleans
   function assertSame(actual, expected) {
     (actual === expected).should.equal(true);
@@ -24,6 +32,131 @@ describe("Cross-realm morphing tests", function () {
     inner.contentDocument.body.innerHTML = "<b>STATE</b>";
     return inner;
   }
+
+  it("morphs outerHTML as content properly when argument is a node from another document", function () {
+    let initial = make("<button>Foo</button>");
+    let iframe = document.createElement("iframe");
+    getWorkArea().append(iframe);
+    let final = iframe.contentDocument.createElement("button");
+    final.textContent = "Bar";
+    Idiomorph.morph(initial, final, { morphStyle: "outerHTML" });
+    initial.outerHTML.should.equal("<button>Bar</button>");
+  });
+
+  it("syncs input value when newContent is a node from another document", function () {
+    let initial = make('<input type="text">');
+    initial.value = "Foo";
+    let iframe = document.createElement("iframe");
+    getWorkArea().append(iframe);
+    let final = iframe.contentDocument.createElement("input");
+    final.setAttribute("type", "text");
+    final.setAttribute("value", "Bar");
+    Idiomorph.morph(initial, final, { morphStyle: "outerHTML" });
+    initial.value.should.equal("Bar");
+  });
+
+  it("preserves id'd element when detached newContent is from another document", function () {
+    let initial = make(`<div><span id="s">Foo</span></div>`);
+    let span = initial.querySelector("span");
+    let final = makeForeign(`<div><div><span id="s">Foo</span></div></div>`);
+    final.remove();
+    Idiomorph.morph(initial, final);
+    initial.outerHTML.should.equal(
+      `<div><div><span id="s">Foo</span></div></div>`,
+    );
+    initial.querySelector("span").should.equal(span);
+  });
+
+  it("preserves id'd element when attached newContent is from another document", function () {
+    let initial = make(`<div><span id="s">Foo</span></div>`);
+    let span = initial.querySelector("span");
+    let final = makeForeign(`<div><div><span id="s">Foo</span></div></div>`);
+
+    Idiomorph.morph(initial, final);
+    initial.outerHTML.should.equal(
+      `<div><div><span id="s">Foo</span></div></div>`,
+    );
+    initial.querySelector("span").should.equal(span);
+  });
+
+  it("morphs template tag contents when newContent is from another document", function () {
+    let initial = make("<template data-old>Foo</template>");
+    let final = makeForeign("<template data-new>Bar</template>");
+    Idiomorph.morph(initial, final);
+    initial.outerHTML.should.equal('<template data-new="">Bar</template>');
+  });
+
+  it("syncs option selectedness when newContent is from another document", function () {
+    let parent = make(
+      `<div><select><option>0</option><option>1</option></select></div>`,
+    );
+    getWorkArea().append(parent);
+    let select = parent.querySelector("select");
+    let final = makeForeign(
+      `<select><option>0</option><option>1</option></select>`,
+    );
+    final.children[1].selected = true;
+    Idiomorph.morph(select, final);
+    select.selectedIndex.should.equal(1);
+  });
+
+  it("syncs textarea value when newContent is from another document", function () {
+    let initial = make("<textarea>Foo</textarea>");
+    initial.value = "dirty";
+    let final = makeForeign("<textarea>Bar</textarea>");
+    Idiomorph.morph(initial, final);
+    initial.value.should.equal("Bar");
+  });
+
+  it("merges head element from another document", function () {
+    let iframe = document.createElement("iframe");
+    getWorkArea().append(iframe);
+    let foreignDocument = iframe.contentDocument;
+    foreignDocument.head.innerHTML = `<meta name="a"><title>Foo</title>`;
+    let newHead = foreignDocument.createElement("head");
+    newHead.innerHTML = `<title>Foo</title><meta name="a">`;
+    Idiomorph.morph(foreignDocument.head, newHead);
+    foreignDocument.head.innerHTML.should.equal(
+      `<meta name="a"><title>Foo</title>`,
+    );
+  });
+
+  it("ignores head element from another document when head.style is none", function () {
+    let iframe = document.createElement("iframe");
+    getWorkArea().append(iframe);
+    let foreignDocument = iframe.contentDocument;
+    foreignDocument.title = "Old";
+    let newHead = foreignDocument.createElement("head");
+    newHead.innerHTML = "<title>New</title>";
+    Idiomorph.morph(foreignDocument.head, newHead, {
+      head: { style: "none" },
+    });
+    foreignDocument.title.should.equal("Old");
+  });
+
+  it("preserves namespaced attributes when newContent is from another document", function () {
+    let initial = make(`<div><svg><use></use></svg></div>`);
+    let final = makeForeign(
+      `<div><svg><use xlink:href="#foo"></use></svg></div>`,
+    );
+    Idiomorph.morph(initial, final);
+    initial
+      .querySelector("use")
+      .getAttributeNS("http://www.w3.org/1999/xlink", "href")
+      .should.equal("#foo");
+  });
+
+  it("morphs a document from another realm properly", function () {
+    let iframe = document.createElement("iframe");
+    getWorkArea().append(iframe);
+    let foreignDocument = iframe.contentDocument;
+    foreignDocument.body.innerHTML = "<p>Foo</p>";
+    Idiomorph.morph(
+      foreignDocument,
+      "<html><head></head><body><p>Bar</p></body></html>",
+    );
+    foreignDocument.body.innerHTML.should.equal("<p>Bar</p>");
+  });
 
   it("creates added nodes in the target's realm", function () {
     let iframe = makeIframe();
